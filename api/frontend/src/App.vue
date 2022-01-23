78fe0eb662cbab38b32a5e2418a8cdc833b0a863
AbdulH <haritani2002@gmail.com>
2022-01-23 22:16:52 +0100

    added overflow hidden to body

--- api/frontend/src/App.vue
+++ api/frontend/src/App.vue
@@ -3,28 +3,30 @@
     <the-nav-bar />
     <div class="container-fluid">
       <div class="row">
-<!--         <div class="col-1"></div>
- -->        <div class="col-12 content"><router-view /></div>
-<!--         <div class="col-1"></div>
- -->      </div>
+        <!--         <div class="col-1"></div>
+ -->
+        <div class="col-12 content"><router-view /></div>
+        <!--         <div class="col-1"></div>
+ -->
+      </div>
     </div>
     <div class="send-clips">
-    <the-send-clip/>
-  </div>
-  <the-footer/>
+      <the-send-clip />
+    </div>
+    <the-footer />
   </div>
 </template>
 
 <script>
 import TheNavBar from "./components/ui/TheNavBar.vue";
-import TheSendClip from './components/ui/TheSendClip.vue';
-import TheFooter from './components/ui/TheFooter.vue';
+import TheSendClip from "./components/ui/TheSendClip.vue";
+import TheFooter from "./components/ui/TheFooter.vue";
 export default {
   name: "App",
   components: {
     "the-nav-bar": TheNavBar,
-    "the-send-clip":TheSendClip,
-    "the-footer":TheFooter,
+    "the-send-clip": TheSendClip,
+    "the-footer": TheFooter,
   },
 };
 </script>
@@ -38,15 +40,16 @@ export default {
 }
 body {
   background-color: #1f1f1f;
-  font-family: 'Mark Pro',Arial, sans-serif;
+  font-family: "Mark Pro", Arial, sans-serif;
+  overflow-x: hidden;
 }
-.content{
-  padding:0;
+.content {
+  padding: 0;
 }
-.card{
+.card {
   cursor: pointer;
 }
-.send-clips{
+.send-clips {
   background-color: #ebebeb;
 }
 </style>
