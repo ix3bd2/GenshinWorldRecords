e887f226e7518c6c65efc552cb4d4a99ed0306b1
AbdulH <haritani2002@gmail.com>
2022-01-29 18:07:43 +0100

    small desgin changes

--- api/frontend/src/components/character/Show.vue
+++ api/frontend/src/components/character/Show.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="item">
     <div class="header-bg-show"></div>
     <div class="header-inv-row"></div>
     <div class="row">
@@ -9,14 +9,22 @@
           <div class="navbar navbar-expand-lg" id="CharcterShowHeader">
             <div class="container">
               <ul class="nav">
-                <li class="nav-item">
+                <a class="navbar-brand" style="  margin-right: 0.1rem
+!important;" href="#"> <img
+                class="element-show-character"
+              :src="item['element']['img']"
+             :style="{ border: getElementBorderColor(item['element']['img']) }"
+            />
+            </a> <li class="nav-item ">
                   <a
                     class="nav-link active active-show"
-                    aria-current="page"
-                    href="#"
-                    >Character</a
+                    href="#" style="padding-top: 13%;
+    padding-left: 0px; font-size:1.3rem;"
+                    >{{ item["name"] }}</a
                   >
                 </li>
+
+               
               </ul>
               <ul class="nav ms-auto mr-5">
                 <li class="nav-item">
@@ -47,13 +55,57 @@
             </div>
           </div>
         </div>
-        <div class="row" v-if="item">
+        <div class="row">
           <img
             id="banner"
             :src="require('@/assets/img/fullBanners/' + item['name'] + '.jpg')"
           />
+          <img
+            class="character-icon-show nopadding"
+            :src="require('@/assets/img/pPicture/' + item['name'] + '.jpg')"
+            v-bind:style="{
+              border: getElementBorderColor(item['element']['name']),
+            }"
+          />
         </div>
-        <div class="row info-section nopadding">sd</div>
+        <div class="row info-section nopadding">
+          <div class="col-md-4 col-sm-12">
+            <h4 class="title-show">Talents</h4>
+            <div class="row">
+              <div class="col-4 nopadding">
+                <img class="talents-show" v-bind:src="item['AAimg']" />
+                <br />
+                10
+              </div>
+              <div class="col-4 nopadding">
+                <img class="talents-show" v-bind:src="item['talentEimg']" />
+                <br />
+                10
+              </div>
+              <div class="col-4 nopadding">
+                <img
+                  class="talents-show"
+                  v-bind:src="item['talentQimg']"
+                /><br />
+                10
+              </div>
+            </div>
+            <h4 class="title-show">Artifacts</h4>
+            <div class="row">
+              <div class="col-4 nopadding">
+                <img class="talents-show" v-bind:src="item['AAimg']" />
+              </div>
+              <div class="col-4 nopadding">
+                <img class="talents-show" v-bind:src="item['talentEimg']" />
+              </div>
+            </div>
+            
+          </div>
+ <div class="col-md-8 col-sm-12">
+            <h4 class="title-show">Stats</h4>
+          </div>
+          <hr />
+        </div>
       </div>
       <div class="col-lg-2 col-md-1"></div>
     </div>
@@ -94,6 +146,16 @@ export default {
 };
 </script>
 <style>
+.character-icon-show {
+  width: 110px;
+  position: absolute;
+  left: 50%;
+  top: 65%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 4px 8px 0 rgba(255, 255, 255, 0.2),
+    0 6px 20px 0 rgba(255, 255, 255, 0.19);
+  border-radius: 50%;
+}
 .active-show {
   color: white;
   transition: 2 ease-in-out;
@@ -103,7 +165,15 @@ export default {
 }
 #CharcterShowHeader {
   background-color: #111111e8;
-  box-shadow: 0 4px 8px 0 rgba(255, 255, 255, 0.2),
+  box-shadow: 0 4px 8px 0 rgba(255, 255, 255, 0.2);
+}
+.title-show {
+  font-size: 2rem;
+  font-weight: 800;
+}
+.talents-show {
+  width: 60px;
+  margin: 2%;
 }
 .header-bg-show {
   background-image: url("../../assets/img/homePage/HomePageHeader.jpeg");
@@ -117,7 +187,7 @@ export default {
   position: absolute;
   z-index: -1;
 }
-.show-container{
+.show-container {
   margin-bottom: 145px;
 }
 #banner {
@@ -133,7 +203,12 @@ export default {
   margin: 0 !important;
 }
 .info-section {
-  background-color:#111111e8;
+  background-color: #111111e8;
+  padding: 3% !important;
+}
+.element-show-character{
+  width: 40px;
+  height: 40px;
 }
 /* .character-banner-show {
   width: 100%;
