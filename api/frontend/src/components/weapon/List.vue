f81304812da9e3535596c579dcf32499d9f505ee
AbdulH <haritani2002@gmail.com>
2022-02-11 19:35:57 +0100

    weapon list desgin

--- api/frontend/src/components/weapon/List.vue
+++ api/frontend/src/components/weapon/List.vue
@@ -1,32 +1,21 @@
 <template>
   <div class="weapons-list">
     <h1>Weapon List</h1>
-
-    <div class="row">
-      <div
-        class="col-md-4 col-lg-3 col-sm-6 col-card"
-        v-for="item in items"
-        :key="item['@id']"
-        data-aos="fade-up"
-        data-aos-duration="4000"
-      >
-        <div class="card" id="WeaponCard">
+    <hr />
+    <div class="row" data-aos="fade-up" data-aos-duration="4000">
+      <div class="col-6 col-md-3 col-lg-2" v-for="item in items" :key="item['@id']">
+        <div id="WeaponCard">
           <div class="card-body">
             <h5 class="card-title">
-              <img :src="item.img">
+              <img id="weapon-img" :src="item.img" />
             </h5>
-            <h6 class="card-subtitle mb-2 text-muted">{{ item.name }}</h6>
-            <p
-              class="card-text"
-            >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            
+            <h6 class="card-subtitle mt-3" style="color:whit!important;">{{ item.name }}</h6>
           </div>
         </div>
       </div>
     </div>
-    <hr>
+    <hr />
     <the-pagination v-if="view" :view="view" @previous="getPage" @next="getPage" />
-
   </div>
 </template>
 
@@ -35,9 +24,9 @@ import { mapActions } from 'vuex';
 import { mapFields } from 'vuex-map-fields';
 import ThePagination from '../ui/ThePagination.vue';
 export default {
-  components:{
-    "the-pagination":ThePagination
-  },  
+  components: {
+    "the-pagination": ThePagination
+  },
   computed: {
     ...mapFields('weapon/del', {
       deletedItem: 'deleted',
@@ -64,16 +53,17 @@ export default {
 <style scoped>
 #WeaponCard {
   margin: 3%;
-  box-shadow: 0 4px 8px 0 rgba(255, 255, 255, 0.2),
-    0 6px 20px 0 rgba(255, 255, 255, 0.19);
   transition: ease-out 0.3s;
-  background-color: #111111;
 }
 #WeaponCard:hover {
   transform: scale(1.05);
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.12), 0 4px 8px rgba(0, 0, 0, 0.06);
 }
-.weapons-list{
-  padding:4%;
+.weapons-list {
+  padding: 4%;
+}
+#weapon-img {
+  -webkit-filter: drop-shadow(1px 1px 0 #f5f5f5d7)
+    drop-shadow(-1px -1px 0 #f5f5f5d7);
+  filter: drop-shadow(1px 1px 0 #f5f5f5d7) drop-shadow(-1px -1px 0 #f5f5f5d7);
 }
 </style>
