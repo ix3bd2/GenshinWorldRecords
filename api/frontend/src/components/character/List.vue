e2a4111e761d83ad73da6c00e4d0cde7d3461389
AbdulH <haritani2002@gmail.com>
2022-02-27 14:05:26 +0100

    small change

--- api/frontend/src/components/character/List.vue
+++ api/frontend/src/components/character/List.vue
@@ -362,6 +362,8 @@ export default {
 .active-filter {
   height: 47px;
   margin: 5px;
+  box-shadow: 0 4px 8px 0 rgba(255, 255, 255, 0.2),
+    0 6px 20px 0 rgba(255, 255, 255, 0.19);
 }
 .input-container {
   position: relative;
