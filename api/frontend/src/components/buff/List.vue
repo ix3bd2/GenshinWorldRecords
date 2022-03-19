735fbaa6bd2d45152c1e6043e063e35e231e3550
AbdulH <haritani2002@gmail.com>
2022-03-19 22:41:43 +0100

    working buff list

--- api/frontend/src/components/buff/List.vue
+++ api/frontend/src/components/buff/List.vue
@@ -2,8 +2,49 @@
   <div class="buffs-list">
     <h1>Buff List</h1>
     <hr />
+    <div class="row">
+      <div class="col-sm-2 mb-1"></div>
+      <div class="col-lg-8 col-md-12">
+        <img
+          v-if="!weapons"
+          class="filter-buff-inactive"
+          v-on:click="filters('weapon')"
+          :src="require('@/assets/img/weapons-icon.webp')"
+        />
+        <img
+          v-if="weapons"
+          class="filter-buff-active"
+          :src="require('@/assets/img/weapons-icon.webp')"
+        />
+        <img
+          class="filter-buff-inactive"
+          v-if="!characters"
+          v-on:click="filters('character')"
+          style="width:45px"
+          :src="require('@/assets/img/characters-icon.webp')"
+        />
+        <img
+          v-if="characters"
+          class="filter-buff-active"
+          style="width:45px"
+          :src="require('@/assets/img/characters-icon.webp')"
+        />
+        <img
+          v-if="!artifacts"
+          class="filter-buff-inactive"
+          v-on:click="filters('artifact')"
+          :src="require('@/assets/img/artifacts-icon.webp')"
+        />
+        <img
+          v-if="artifacts"
+          class="filter-buff-active"
+          :src="require('@/assets/img/artifacts-icon.webp')"
+        />
+      </div>
+      <div class="col-2" style="margin-right:7px"></div>
+    </div>
     <div class="row" data-aos="fade-up" data-aos-duration="4000">
-      <div class="col-6 col-md-3 col-lg-2" v-for="item in items" :key="item['@id']">
+      <div class="col-6 col-md-3 col-lg-2" v-for="item in resultQuery" :key="item['@id']">
         <div class="buff-card" v-if="item.character">
           <div class="card-body">
             <router-link id="BuffRouter" :to="{ name: 'BuffShow', params: { id: item['@id'] } }">
@@ -11,7 +52,6 @@
                 <img
                   id="CharacterElementBuffsList"
                   v-bind:src="item.character['element']['img']"
-                  alt="Card image cap"
                   v-bind:style="{ border: getElementBorderColor(item.character['element']['name']) }"
                 />
                 <img
@@ -21,13 +61,19 @@
                   v-bind:style="{ border: getElementBorderColor(item.character['element']['name']) }"
                 />
               </h5>
-              <h6 class="card-subtitle mt-3" style="color:whit!important;">{{ filterName(item.character.name) }}</h6>
+              <h6
+                class="card-subtitle mt-3"
+                style="color:whit!important;"
+              >{{ filterName(item.character.name) }}</h6>
             </router-link>
           </div>
         </div>
         <div class="buff-card" v-if="item.artifact">
           <div class="card-body">
-            <router-link id="BuffRouter" :to="{ name: 'ArtifactShow', params: { id: item.artifact['@id'] } }">
+            <router-link
+              id="BuffRouter"
+              :to="{ name: 'ArtifactShow', params: { id: item.artifact['@id'] } }"
+            >
               <h5 class="card-title">
                 <img :src="item.artifact.img" style="width:106px;" id="BuffImg" />
               </h5>
@@ -37,7 +83,10 @@
         </div>
         <div class="buff-card" v-if="item.weapon">
           <div class="card-body">
-            <router-link id="BuffRouter" :to="{ name: 'WeaponShow', params: { id: item.weapon['@id'] } }">
+            <router-link
+              id="BuffRouter"
+              :to="{ name: 'WeaponShow', params: { id: item.weapon['@id'] } }"
+            >
               <h5 class="card-title">
                 <img :src="item.weapon.img" style="width:106px;" id="BuffImg" />
               </h5>
@@ -60,6 +109,14 @@ import { charactersMixin } from "../../mixins/charactersMixin";
 
 export default {
   mixins: [charactersMixin],
+  data() {
+    return {
+      characters: false,
+      weapons: false,
+      artifacts: false,
+      searchQuery: null
+    }
+  },
   components: {
     "the-pagination": ThePagination
   },
@@ -73,7 +130,26 @@ export default {
       isLoading: 'isLoading',
       view: 'view',
     }),
-
+    resultQuery() {
+      if (this.searchQuery && this.characters) {
+        return this.items.filter((item) => {
+          return this.searchQuery.toLowerCase().split(' ').every(v => item.img.toLowerCase().includes(v))
+        })
+      }
+      if (this.searchQuery && this.weapons) {
+        return this.items.filter((item) => {
+          return this.searchQuery.toLowerCase().split(' ').every(v => item.img.toLowerCase().includes(v))
+        })
+      }
+      if (this.searchQuery && this.artifacts) {
+        return this.items.filter((item) => {
+          return this.searchQuery.toLowerCase().split(' ').every(v => item.img.toLowerCase().includes(v))
+        })
+      }
+      else {
+        return this.items;
+      }
+    }
   },
 
   mounted() {
@@ -87,6 +163,21 @@ export default {
     filterName(name) {
       return name.replace("-", " ");
     },
+    filters(filter) {
+      this.weapons = false
+      this.characters = false
+      this.artifacts = false
+      this.searchQuery = filter
+      if (filter == "character") {
+        this.characters = true;
+      }
+      if (filter == "artifact") {
+        this.artifacts = true;
+      }
+      if (filter == "weapon") {
+        this.weapons = true;
+      }
+    }
   },
 };
 </script>
@@ -124,5 +215,17 @@ export default {
 #BuffRouter {
   color: white;
   text-decoration: none;
+}
+.filter-buff-inactive {
+  -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+  filter: grayscale(100%);
+  transition: ease-out 0.3s;
+}
+.filter-buff-inactive:hover {
+  transform: scale(1.15);
+  filter: none;
+}
+.filter-buff-active {
+  transform: scale(1.15);
 }
 </style>
--- api/frontend/src/components/ui/TheNavBar.vue
+++ api/frontend/src/components/ui/TheNavBar.vue
@@ -1,41 +1,47 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-dark ">
-  <div class="container-fluid">
- <a class="navbar-brand" style="padding-top: 0rem; 
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" style="padding-top: 0rem; 
      margin-right: 0rem;" href="/">
         <img
           :src="require('@/assets/img/logo.png/')"
           style="width: 85px;padding-top: 0rem; 
      margin-right: 0rem;"
         />
-      </a>    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/characters">Characters</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/">Spiral Teams</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/weapons">Weapons</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/artifacts">Artifacts</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/buffs">Buffs</a>
-        </li>
-      </ul>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+         
+          <li class="nav-item">
+            <a class="nav-link" href="/characters"><img class="navbar-items-icon" src="@/assets/img/characters-icon.webp" />&nbsp;Characters</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/"><img class="navbar-items-icon" src="@/assets/img/spiral-abyss-icon.webp" />Spiral Teams</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/weapons"><img class="navbar-items-icon" src="@/assets/img/weapons-icon.webp" />Weapons</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/artifacts"><img class="navbar-items-icon" src="@/assets/img/artifacts-icon.webp" />Artifacts</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/buffs"><img class="navbar-items-icon" src="@/assets/img/atk-up-icon.webp" />Buffs</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
-
+  </nav>
 </template>
 
 <script>
@@ -45,8 +51,13 @@ export default {
 </script>
 
 <style>
-.navbar{
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+.navbar {
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
   background-color: #111111;
+}
+.navbar-items-icon{
+  width: 26px;
 }
 </style>
