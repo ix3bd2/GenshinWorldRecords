a2d4bd18cc284a4d63d8812c75ba23a87ed1ede8
AbdulH <haritani2002@gmail.com>
2022-03-20 15:44:06 +0100

    working list

--- api/frontend/src/components/buff/Show.vue
+++ api/frontend/src/components/buff/Show.vue
@@ -1,84 +1,46 @@
 <template>
-  <div>
-    <h1>Show {{ item && item['@id'] }}</h1>
-
-    <div
-      v-if="isLoading"
-      class="alert alert-info"
-      role="status">Loading...</div>
-    <div
-      v-if="error"
-      class="alert alert-danger"
-      role="alert">
-      <span
-        class="fa fa-exclamation-triangle"
-        aria-hidden="true">{{ error }}</span>
+   <div v-if="item" class="buff-show">
+    <div>
+      <img
+                  id="CharacterElementBuffsList"
+                  v-bind:src="item.character['element']['img']"
+                  v-bind:style="{ border: getElementBorderColor(item.character['element']['name']) }"
+                />
+                <img
+                id="buffImgShow"
+                  :src="require('@/assets/img/pPicture/' + item.character['name'] + '.jpg')"
+                  style="width:106px"
+                  class="character-buff-icon"
+                  v-bind:style="{ border: getElementBorderColor(item.character['element']['name']) }"
+                />
+      <h5 class="mt-2">{{ item.character.name }}</h5>
     </div>
-    <div
-      v-if="deleteError"
-      class="alert alert-danger"
-      role="alert">
-      <span
-        class="fa fa-exclamation-triangle"
-        aria-hidden="true">{{ deleteError }}</span>
+    <div class="row">
+      <div class="col-lg-2"></div>
+      <div class="col-lg-8 col-md-10 col-sm-12 text-center mt-3">
+        <ul>
+          <li class="mb-4">
+            <div
+              style="width: 100%; height: 15px; border-bottom: 1px solid #565656; text-align: center"
+            >
+              <span style="font-weight: 900;background-color:#1f1f1f; padding: 0 10px;">Provided buffs</span>
+            </div>
+            <div class="mt-3">{{  }}</div>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-2"></div>
     </div>
-    <div
-      v-if="item"
-      class="table-responsive">
-      <table class="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th>Field</th>
-            <th>Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">img</th>
-            <td>{{ item['img'] }}</td>
-          </tr>
-          <tr>
-            <th scope="row">description</th>
-            <td>{{ item['description'] }}</td>
-          </tr>
-          <tr>
-            <th scope="row">character</th>
-            <td>{{ item['character'] }}</td>
-          </tr>
-          <tr>
-            <th scope="row">weapon</th>
-            <td>{{ item['weapon'] }}</td>
-          </tr>
-          <tr>
-            <th scope="row">aritfact</th>
-            <td>{{ item['aritfact'] }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
-    <router-link
-      :to="{ name: 'BuffList' }"
-      class="btn btn-primary">
-      Back to list
-    </router-link>
-    <router-link
-      v-if="item"
-      :to="{ name: 'BuffUpdate', params: { id: item['@id'] } }"
-      class="btn btn-warning">
-      Edit
-    </router-link>
-    <button
-      class="btn btn-danger"
-      @click="deleteItem(item)">Delete</button>
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex';
 import { mapFields } from 'vuex-map-fields';
+import { charactersMixin } from "../../mixins/charactersMixin";
 
 export default {
+  mixins: [charactersMixin],
   computed: {
     ...mapFields('buff/del', {
       deleteError: 'error',
@@ -113,3 +75,24 @@ export default {
   },
 };
 </script>
+<style scoped>
+.buff-show {
+  padding: 4%;
+}
+#buffImgShow {
+
+  filter: drop-shadow(1px 1px 0 #f5f5f5d7) drop-shadow(-1px -1px 0 #f5f5f5d7);
+}
+#CharacterElementBuffsList {
+  width: 30px;
+  position: absolute;
+  z-index: 1;
+  background-color: #19232f;
+  border-radius: 50%;
+  box-shadow: 0 4px 8px 0 rgb(255 255 255 / 20%),
+    0 6px 20px 0 rgb(255 255 255 / 19%);
+}
+.character-buff-icon {
+  border-radius: 50%;
+}
+</style>
