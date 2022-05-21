a71c095cf3a6308a760b6cd5b5616cfd33251507
AbdulH <haritani2002@gmail.com>
2022-05-21 17:13:48 +0200

    small change

--- frontend/src/components/home/HomePage.vue
+++ frontend/src/components/home/HomePage.vue
@@ -2,7 +2,7 @@
   <div class="home-header">
     <div class="header-bg">
       <div class="row top3-dmg">
-        
+          <div class="col" v-for="item in fullData" :key="item['@id']">{{ item.character.name }}</div>
       </div>
     </div>
     <home-content />
@@ -10,10 +10,22 @@
 </template>
 <script>
 import HomeContent from "./HomeContent.vue";
+import axios from 'axios';
+import { ENTRYPOINT } from '../../config/entrypoint';
+
 export default {
   components: {
     "home-content": HomeContent,
   },
+  data() {
+    return {
+      fullData: null,
+    };
+  },
+  created(){
+        axios.get(ENTRYPOINT + '/top3dmgs?pagination=false')
+      .then(response => (this.fullData = response.data['hydra:member']))
+  }
 };
 </script>
 
