5cd536dc5af90f8e1e74fe77101f567d4ffdfada
AbdulH <haritani2002@gmail.com>
2022-11-19 16:22:58 +0100

    upwork test

--- frontend/src/components/home/HomeGenshinTweets.vue
+++ frontend/src/components/home/HomeGenshinTweets.vue
@@ -5,11 +5,14 @@
       <div class="col-lg-7 col-md-12">
         <h2 class="title-video mb-4">Latest video</h2>
         <iframe
-          width="100%"
-          height="90%"
-          class="youtube-iframe"
-          :src="'https://www.youtube.com/embed/' + embYoutube"
-        ></iframe>
+    id="existing-iframe-example"
+    width="640" height="360"
+    src="https://www.youtube.com/embed/IzB09UCTo8s?autoplay=1&mute=1&enablejsapi=1&modestbranding=1"
+    frameborder="0"
+    style="border: solid 4px #37474F"
+></iframe>
+
+        
       </div>
 
       <div class="col-lg-1 col-md-12 social-split">
